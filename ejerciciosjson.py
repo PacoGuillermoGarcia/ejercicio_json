8902b759a98ef031d0c54f9d1a11a1fbb3d9af82
pakotoes3@gmail.com <paco@Paco>
2019-03-10 16:09:39 +0100

    estoy realizando el ejercicio1 de json

---
+++ ejerciciosjson.py
@@ -0,0 +1,29 @@
+#1. Listar tipos de documentos adjuntos
+
+def Tiposadjuntos(datos):
+	lista=[]
+	listaadjuntos=[]
+	listatipos=[]
+	for documento in datos["expediente"]["anuncios"]["anuncio"]:
+		lista.append(documento)
+	for elem in lista:
+		if type(elem["documentoAdjunto"]) == list:
+			for elem2 in elem["documentoAdjunto"]:
+				listatipos.append(elem2["tipo"])
+		elif type(elem["documentoAdjunto"])==dict:
+			listatipos.append(elem["tipo"])
+	return listatipos
+
+
+#2. Pedir por teclado el tipo de anuncio y cuenta los anuncios relacionados
+
+#3. Pedir por teclado la denominacion del contrato y mostrar la descripción de los CPV
+
+#4. Pedir por teclado la fecha de publicación de un anuncio relacionado y te muestra la referencia de publicación del anuncio principal
+
+#5. Pedir por teclado la fecha de publicación de un anuncio y te muestra el enlace, el tipo y la descripción de los documentos adjuntos
+import json
+with open ("anuncios.json") as fichero:
+	datos=json.load(fichero)
+	for elem in Tiposadjuntos(datos):
+		print(elem)
