3b27603074c7203eb42e19b38a85617b80c1caf3
pakotoes3@gmail.com <paco@Paco>
2019-03-10 17:39:24 +0100

    He realizando todos los ejercicios de json

--- ejerciciosjson.py
+++ ejerciciosjson.py
@@ -58,11 +58,43 @@ def datosdocumentosadjuntos(cad,datos):
 import json
 with open ("anuncios.json") as fichero:
 	datos=json.load(fichero)
+	while True:
+		print('''
+			1.-Tipos datos adjuntos
+			2.-Contador Anuncios Relacionados
+			3.-Descripcion CPVs
+			4.-Referencias Anuncios Relacionados
+			5.-Datos de los Documentos Adjuntos 
+			0.-Salir''')
+		opcion=input("Dime la opcion: ")
+		if opcion=="1":
+			print(Tiposadjuntos(datos))
+		elif opcion=="2":
+			cad=input("Dime el tipo de contratos(Servicios o Suministros): ")
+			while cad not in ("Servicios","Suministros"):
+				print("No tengo contratos con ese Tipo")
+				cad=input("Dime el tipo de contratos(Servicios o Suministros): ")
+			print(contarrelacionados(cad,datos))
+		elif opcion=="3":
+			cad=input("Dime la denominacion del contrato(ALMACENAMIENTO,Servicio,Suministro): ")
+			while cad not in ("ALMACENAMIENTO","Servicio","Suministro"):
+				print("No tengo contratos de esa denominacion")
+				cad=input("Dime la denominacion del contrato(ALMACENAMIENTO,Servicio,Suministro): ")
+			print(descCPV(cad,datos))
+		elif opcion=="4":
+			cad=input("Dime la fecha de un anuncio relacionado(dia/mes/año): ")
+			print(referenciarelacionado(cad,datos))
+		elif opcion=="5":
+			cad=input("Dime la fecha de publicacion de un anuncio(dia/mes/año): ")
+			print(datosdocumentosadjuntos(cad,datos))
+		elif opcion=="0":
+			print("Adios")
+			break
 	#cad=input("Dime el tipo de contratos(Servicios o Suministros: ")
 	#print(contarrelacionados(cad,datos))
 	#cad=input("Dime la denominacion del contrato(ALMACENAMIENTO,Servicio,Suminitro): ")
 	#print(descCPV(cad,datos))
 	#cad=input("Dime la fecha de un anuncio relacionado: ")
 	#print(referenciarelacionado(cad,datos))
-	cad=input("Dime la fecha de publicacion de un anuncio: ")
-	print(datosdocumentosadjuntos(cad,datos))
+	#cad=input("Dime la fecha de publicacion de un anuncio: ")
+	#print(datosdocumentosadjuntos(cad,datos))
